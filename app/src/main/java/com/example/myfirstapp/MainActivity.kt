4fa83160d72904a7d8b5508af1c6de53efa00659
Alan Baldwin <baldwingaming1979@gmail.com>
2021-12-14 22:53:01 +0000

    Added conditions to change text colour

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myfirstapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +22,31 @@ class MainActivity : AppCompatActivity() {
             timesClicked += 1
             textBox.text = timesClicked.toString()
             when (timesClicked) {
-                5 -> Toast.makeText(this, "You made it above 5!", Toast.LENGTH_LONG).show()
-                10 -> Toast.makeText(this, "10, Well done", Toast.LENGTH_LONG).show()
-                15 -> Toast.makeText(this, "So many numbers", Toast.LENGTH_LONG).show()
+                5 -> {
+                    Toast.makeText(this, "You made it above 5!", Toast.LENGTH_LONG).show()
+                    textBox.setTextColor(Color.parseColor("#ffff00"))
+                }
+                10 -> {
+                    Toast.makeText(this, "10 Well done", Toast.LENGTH_LONG).show()
+                    textBox.setTextColor(Color.parseColor("#bf00ff"))
+                }
+                15 -> {
+                    Toast.makeText(this, "Keep going", Toast.LENGTH_LONG).show()
+                    textBox.setTextColor(Color.parseColor("#ffbf00"))
+                }
+                20 -> {
+                    Toast.makeText(this, "I have lost count", Toast.LENGTH_LONG).show()
+                    textBox.setTextColor(Color.parseColor("#1C1D1F"))
+                }
+            }
+
+            btnRemoveClickMe.setOnClickListener {
+                timesClicked = 0
+                textBox.text = timesClicked.toString()
+                Toast.makeText(this, "Reset back to 0", Toast.LENGTH_LONG).show()
+                textBox.setTextColor(Color.parseColor("#ff0000"))
 
             }
-        }
-
-        btnRemoveClickMe.setOnClickListener {
-            timesClicked = 0
-            textBox.text = timesClicked.toString()
-            Toast.makeText(this, "Reset back to 0", Toast.LENGTH_LONG).show()
-
         }
     }
 }
